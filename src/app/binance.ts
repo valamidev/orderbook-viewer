1143ae1ef6c77fa2781e27f21d195b57fcf1e6ac
T <52665225+valamidev@users.noreply.github.com>
2021-04-15 19:33:46 +0200

    Version 0.1

--- src/app/binance.ts
+++ src/app/binance.ts
@@ -2,7 +2,15 @@ import axios from "axios";
 
 const BASE_URL = `https://api.binance.com`;
 
-export const GetOrderBook = async (symbol = 'XRPUSDT', limit = 5000): Promise<{ asks: any[], bids: any[] }> => {
+export const GetExchangeInfo = async (): Promise<any> => {
+    // /api/v3/exchangeInfo
+
+    const response = await axios.get(`${BASE_URL}/api/v3/exchangeInfo`);
+
+    return response.data;
+}
+
+export const GetOrderBook = async (symbol = 'ETHUSDT', limit = 5000): Promise<{ asks: any[], bids: any[] }> => {
     // /api/v3/depth
 
     const response = await axios.get(`${BASE_URL}/api/v3/depth?symbol=${symbol}&limit=${limit}`);
